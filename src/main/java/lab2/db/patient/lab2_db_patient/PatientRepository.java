fa3507cab935d8c55e0c11a4266172f820753076
Fabian <haslinger.fabian1503@gmail.com>
2024-09-17 15:21:48 +0200

    First and Final commit

---
+++ src/main/java/lab2/db/patient/lab2_db_patient/PatientRepository.java
@@ -0,0 +1,8 @@
+package lab2.db.patient.lab2_db_patient;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PatientRepository extends CrudRepository<Patient, Long> {
+
+
+}
---
+++ src/test/java/lab2/db/patient/lab2_db_patient/Lab2DbPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package lab2.db.patient.lab2_db_patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2DbPatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
